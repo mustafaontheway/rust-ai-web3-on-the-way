80e289dbfefbe1fdde0b0f0dedca9e9cb339d1f6
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-09-30 11:30:13 +0300

    Create part-z-0003-float2.rs

---
+++ part-z-0003-float2.rs
@@ -0,0 +1,13 @@
+// // Make it work in two distinct ways
+// fn main() {
+//     assert!(0.1+0.2==0.3);
+
+//     println!("Success!");
+// }
+
+// Make it work in two distinct ways
+fn main() {
+    assert!(0.1_f32+0.2_f32==0.3_f32);
+
+    println!("Success!");
+}
