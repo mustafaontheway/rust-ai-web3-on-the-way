ab9905c57a13222c2035da4503f389cff9b4a0da
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-09-30 20:16:22 +0300

    Create part-z-0008-unused-variables.rs

---
+++ part-z-0008-unused-variables.rs
@@ -0,0 +1,13 @@
+// // Fix the warning, none of the solutions is to remove the line let x = 1
+// fn main() {
+//     let x = 1; 
+// }
+
+// // Warning: unused variable: `x`
+
+// Fix the warning, none of the solutions is to remove the line let x = 1
+fn main() {
+    let _x = 1; 
+}
+
+// Warning: unused variable: `x`
