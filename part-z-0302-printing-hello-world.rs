35b2a5431d417cd2467b4a15e32c38a9647dd8cf
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-09-30 10:41:17 +0300

    Create part-z-0302-printing-hello-world.rs

---
+++ part-z-0302-printing-hello-world.rs
@@ -0,0 +1,10 @@
+/*
+Your task
+Write a program that prints "Hello, World!" to the console using the println! macro.
+
+*/
+
+pub fn hello_world() {
+    // Use the println! macro to print "Hello, world!" to the console
+    println!("Hello, World!")
+}
