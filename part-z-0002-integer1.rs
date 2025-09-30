dd1fd665300795803629ad75be2669e7cd9950ce
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-09-30 11:16:14 +0300

    Create part-z-0002-integer1.rs

---
+++ part-z-0002-integer1.rs
@@ -0,0 +1,23 @@
+// // Remove something to make it work
+// fn main() {
+//     let x: i32 = 5;
+//     let mut y: u32 = 5;
+
+//     y = x;
+    
+//     let z = 10; // Type of z ? 
+
+//     println!("Success!");
+// }
+
+// Remove something to make it work
+fn main() {
+    let x: i32 = 5;
+    let mut y = 5;
+
+    y = x;
+    
+    let z = 10; // Type of z ? 
+
+    println!("Success!");
+}
