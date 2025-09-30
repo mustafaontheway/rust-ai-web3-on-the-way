3de1ac8cd34ea26eb80d2dad272f26f6cbf3f923
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-09-30 20:01:02 +0300

    Create part-z-0006-scope1.rs

---
+++ part-z-0006-scope1.rs
@@ -0,0 +1,20 @@
+// // Fix the error below with least amount of modification
+// fn main() {
+//     let x: i32 = 10;
+//     {
+//         let y: i32 = 5;
+//         println!("Inner scope value of x is {} and value of y is {}", x, y);
+//     }
+//     println!("Outer scope value of x is {} and value of y is {}", x, y); 
+// }
+
+// Fix the error below with least amount of modification
+fn main() {
+    let x: i32 = 10;
+    {
+        let y: i32 = 5;
+        println!("Inner scope value of x is {} and value of y is {}", x, y);
+    }
+    let y: i32 = 100000;
+    println!("Outer scope value of x is {} and value of y is {}", x, y); 
+}
