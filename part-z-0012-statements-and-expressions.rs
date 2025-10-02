c4922a361ac71dd4e273a38751b6189f2e573546
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-10-02 10:16:37 +0300

    Create part-z-0012-statements-and-expressions.rs

---
+++ part-z-0012-statements-and-expressions.rs
@@ -0,0 +1,24 @@
+// // Make it work with two ways
+// fn main() {
+//    let v = {
+//        let mut x = 1;
+//        x += 2
+//    };
+
+//    assert_eq!(v, 3);
+
+//    println!("Success!");
+// }
+
+// Make it work with two ways
+fn main() {
+   let v = {
+       let mut x = 1;
+       x += 2;
+       x
+   };
+
+   assert_eq!(v, 3);
+
+   println!("Success!");
+}
