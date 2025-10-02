f4c4e86b05de4062c9732023cba86857835d31f6
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-10-02 10:07:01 +0300

    Create part-z-0011-bool1.rs

---
+++ part-z-0011-bool1.rs
@@ -0,0 +1,19 @@
+// // Make println! work
+// fn main() {
+//     let _f: bool = false;
+
+//     let t = true;
+//     if !t {
+//         println!("Success!");
+//     }
+// } 
+
+// Make println! work
+fn main() {
+    let _f: bool = false;
+
+    let t = !true;
+    if !t {
+        println!("Success!");
+    }
+} 
