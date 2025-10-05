0acfd30680e6639926fd26e887dbee2d6e59121f
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-06 02:56:07 +0300

    Create part-z-0014-ownership7.rs

---
+++ part-z-0014-ownership7.rs
@@ -0,0 +1,23 @@
+// fn main() {
+//     let x = Box::new(5);
+    
+//     let ...      // update this line, don't change other lines!
+    
+//     *y = 4;
+    
+//     assert_eq!(*x, 5);
+
+//     println!("Success!");
+// }
+
+fn main() {
+    let x = Box::new(5);
+    
+    let mut y = Box::new(11);      // update this line, don't change other lines!
+    
+    *y = 4;
+    
+    assert_eq!(*x, 5);
+
+    println!("Success!");
+}
