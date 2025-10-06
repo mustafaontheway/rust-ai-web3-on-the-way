b316bc098f6435ad9feb39bf6276833fac78799f
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-06 21:23:18 +0300

    Create part-z-0019-tuple2.rs

---
+++ part-z-0019-tuple2.rs
@@ -0,0 +1,15 @@
+// // Make it work
+// fn main() {
+//     let t = ("i", "am", "sunface");
+//     assert_eq!(t.1, "sunface");
+
+//     println!("Success!");
+// }
+
+// Make it work
+fn main() {
+    let t = ("i", "am", "sunface");
+    assert_eq!(t.1, "am");
+
+    println!("Success!");
+}
