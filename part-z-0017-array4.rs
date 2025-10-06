f07b6c060839a1b952f53e9d163d444a8e7c4295
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-06 19:56:14 +0300

    Create part-z-0017-array4.rs

---
+++ part-z-0017-array4.rs
@@ -0,0 +1,13 @@
+// fn main() {
+//     // Fix the error
+//     let _arr = [1, 2, '3'];
+
+//     println!("Success!");
+// }
+
+fn main() {
+    // Fix the error
+    let _arr = [1, 2, 3];
+
+    println!("Success!");
+}
