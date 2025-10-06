7ca691e5f3123a74d6a3f236024a3d3f4b999edc
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-06 03:07:03 +0300

    Create part-z-0014-ownership9.rs

---
+++ part-z-0014-ownership9.rs
@@ -0,0 +1,20 @@
+// fn main() {
+//    let t = (String::from("hello"), String::from("world"));
+
+//     // Fill the blanks
+//     let (__, __) = __;
+
+//     println!("{:?}, {:?}, {:?}", s1, s2, t); // -> "hello", "world", ("hello", "world")
+// }
+
+
+
+fn main() {
+   let t = (String::from("hello"), String::from("world"));
+
+    // Fill the blanks
+    //let (s1, s2) = &t;
+    let (ref s1, ref s2) = t;
+
+    println!("{:?}, {:?}, {:?}", s1, s2, t); // -> "hello", "world", ("hello", "world")
+}
