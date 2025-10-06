26036eb6543839d977e8756bce138dc68d455321
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-06 21:32:15 +0300

    Create part-z-0019-tuple5.rs

---
+++ part-z-0019-tuple5.rs
@@ -0,0 +1,25 @@
+// fn main() {
+//     let (x, y, z);
+
+//     // Fill the blank
+//     __ = (1, 2, 3);
+    
+//     assert_eq!(x, 3);
+//     assert_eq!(y, 1);
+//     assert_eq!(z, 2);
+
+//     println!("Success!");
+// }
+
+fn main() {
+    let (x, y, z);
+
+    // Fill the blank
+    (y, z, x) = (1, 2, 3);
+    
+    assert_eq!(x, 3);
+    assert_eq!(y, 1);
+    assert_eq!(z, 2);
+
+    println!("Success!");
+}
