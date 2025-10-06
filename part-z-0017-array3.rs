d5f40c6b54e540788e807040f4a932667480bec1
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-06 19:55:15 +0300

    Create part-z-0017-array3.rs

---
+++ part-z-0017-array3.rs
@@ -0,0 +1,19 @@
+// fn main() {
+//     // Fill the blank
+//     let list: [i32; 100] = __ ;
+
+//     assert!(list[0] == 1);
+//     assert!(list.len() == 100);
+
+//     println!("Success!");
+// }
+
+fn main() {
+    // Fill the blank
+    let list: [i32; 100] = [1; 100] ;
+
+    assert!(list[0] == 1);
+    assert!(list.len() == 100);
+
+    println!("Success!");
+}
