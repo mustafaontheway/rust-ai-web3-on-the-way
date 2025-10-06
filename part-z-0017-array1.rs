09a8bfe6a92ee5c65ada1342145162a25e00049a
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-06 19:50:14 +0300

    Create part-z-0017-array1.rs

---
+++ part-z-0017-array1.rs
@@ -0,0 +1,19 @@
+// fn main() {
+//     // Fill the blank with proper array type
+//     let arr: __ = [1, 2, 3, 4, 5];
+
+//     // Modify the code below to make it work
+//     assert!(arr.len() == 4);
+
+//     println!("Success!");
+// }
+
+fn main() {
+    // Fill the blank with proper array type
+    let arr: [u8; 5] = [1, 2, 3, 4, 5];
+
+    // Modify the code below to make it work
+    assert!(arr.len() == 5);
+
+    println!("Success!");
+}
