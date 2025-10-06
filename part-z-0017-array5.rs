21f9ed4f90d92df81de3fa41106648c17667e1ac
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-06 19:57:38 +0300

    Create part-z-0017-array5.rs

---
+++ part-z-0017-array5.rs
@@ -0,0 +1,19 @@
+// fn main() {
+//     let arr = ['a', 'b', 'c'];
+    
+//     let ele = arr[1]; // Only modify this line to make the code work!
+
+//     assert!(ele == 'a');
+
+//     println!("Success!");
+// }
+
+fn main() {
+    let arr = ['a', 'b', 'c'];
+    
+    let ele = arr[0]; // Only modify this line to make the code work!
+
+    assert!(ele == 'a');
+
+    println!("Success!");
+}
