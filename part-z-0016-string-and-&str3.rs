b0f54aa512ab026aba18c70b987c18f924a0e5b3
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-06 14:04:31 +0300

    Create part-z-0016-string-and-&str3.rs

---
+++ part-z-0016-string-and-&str3.rs
@@ -0,0 +1,21 @@
+// // Fill the blank
+// fn main() {
+//     let mut s = __;
+//     s.push_str("hello, world");
+//     s.push('!');
+
+//     assert_eq!(s, "hello, world!");
+
+//     println!("Success!");
+// }
+
+// Fill the blank
+fn main() {
+    let mut s = "".to_string();
+    s.push_str("hello, world");
+    s.push('!');
+
+    assert_eq!(s, "hello, world!");
+
+    println!("Success!");
+}
