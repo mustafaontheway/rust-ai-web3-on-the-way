324b78ae04d5b4638d9e7f26354c0dc49a3959c3
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-06 13:58:58 +0300

    Create part-z-0016-string-literal1.rs

---
+++ part-z-0016-string-literal1.rs
@@ -0,0 +1,13 @@
+// // Fix error without adding new line
+// fn main() {
+//     let s: str = "hello, world";
+
+//     println!("Success!");
+// }
+
+// Fix error without adding new line
+fn main() {
+    let s: &str = "hello, world";
+
+    println!("Success!");
+}
