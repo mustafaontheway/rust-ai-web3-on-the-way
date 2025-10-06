44ed8002d68e830999945f01bbef352d2461b1c9
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-06 14:14:08 +0300

    Create part-z-0016-string-and-&str6.rs

---
+++ part-z-0016-string-and-&str6.rs
@@ -0,0 +1,18 @@
+// // You can only concat a String with &str, and String's ownership can be moved to another variable.
+// // Fix errors without removing any line
+// fn main() {
+//     let s1 = String::from("hello,");
+//     let s2 = String::from("world!");
+//     let s3 = s1 + s2; 
+//     assert_eq!(s3, "hello,world!");
+//     println!("{}", s1);
+// }
+
+// Fix errors without removing any line
+fn main() {
+    let s1 = String::from("hello,");
+    let s2 = String::from("world!");
+    let s3 = s1.clone() + &s2; 
+    assert_eq!(s3, "hello,world!");
+    println!("{}", s1);
+}
