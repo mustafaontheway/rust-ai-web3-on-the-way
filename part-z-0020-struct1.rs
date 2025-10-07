dec6d8eb768fb61e2128061c40938f8e6a5245ba
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-07 13:03:03 +0300

    Create part-z-0020-struct1.rs

---
+++ part-z-0020-struct1.rs
@@ -0,0 +1,32 @@
+// // Fix the error
+// struct Person {
+//     name: String,
+//     age: u8,
+//     hobby: String
+// }
+// fn main() {
+//     let age = 30;
+//     let p = Person {
+//         name: String::from("sunface"),
+//         age,
+//     };
+
+//     println!("Success!");
+// } 
+
+// Fix the error
+struct Person {
+    name: String,
+    age: u8,
+    hobby: String
+}
+fn main() {
+    let age = 30;
+    let p = Person {
+        name: String::from("sunface"),
+        hobby: "Some hobbies".to_string(),
+        age: age,
+    };
+
+    println!("Success!");
+} 
