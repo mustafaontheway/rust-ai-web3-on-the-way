66cf68121165b1d0f8cf446775ea780861d94be2
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-11 10:20:09 +0300

    Create part-w-0001-shadowing1.rs

---
+++ part-w-0001-shadowing1.rs
@@ -0,0 +1,24 @@
+fn main() {
+
+    let data: u16 = 2025;
+
+    println!("{data}");
+
+    let data: f32 = 22.32;
+
+    println!("{data}");
+
+    let data = "11.23";
+
+    println!("{data}");
+
+    let mut data: i32 = -500_000;
+
+    println!("{data}");
+
+    data = 210;
+
+    println!("{data}")
+
+}
+
