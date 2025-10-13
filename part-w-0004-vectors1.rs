9c4ab834f10223d6847aa28ebb1469d082e48a21
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-13 10:38:36 +0300

    Create part-w-0004-vectors1.rs

---
+++ part-w-0004-vectors1.rs
@@ -0,0 +1,8 @@
+fn main() {
+
+    let last_five_years: Vec<u16> = (2021..2026).collect();
+
+    println!("Last five years: {:?}", last_five_years) // Last five years: [2021, 2022, 2023, 2024, 2025]
+
+}
+
