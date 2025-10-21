edf80215b37c4e7d9eb7dc0f2affc86c9bef5422
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-21 21:13:07 +0300

    Create part-y-0002-vector-creation.rs

---
+++ part-y-0002-vector-creation.rs
@@ -0,0 +1,10 @@
+fn main() {
+
+    let my_vec: Vec<&str> = Vec::new();
+
+    println!("Empty vector: {:?}", my_vec); // Empty vector: []
+
+    let _years: Vec<u16> = vec![2012, 2021, 2026];
+
+}
+
