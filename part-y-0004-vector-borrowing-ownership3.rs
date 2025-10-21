d1bc9a30897ab2c4fa3cee01023c80149a7e07fb
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-21 21:41:28 +0300

    Create part-y-0004-vector-borrowing-ownership3.rs

---
+++ part-y-0004-vector-borrowing-ownership3.rs
@@ -0,0 +1,16 @@
+fn main() {
+
+    let city1 = "İzmir";
+    let city2 = "Ankara";
+    let city3 = "İstanbul";
+
+    let big_cities = vec![city1, city2, city3];
+
+    println!("{big_cities:?}");
+
+    println!("İzmir: {city1}"); 
+
+}
+
+// ["İzmir", "Ankara", "İstanbul"]
+// İzmir: İzmir
