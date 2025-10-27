c88ee56c18a858dcaeac1c18548fa2a19971b711
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-27 09:00:42 +0300

    Create part-y-0015-hashmap-overwrite1.rs

---
+++ part-y-0015-hashmap-overwrite1.rs
@@ -0,0 +1,23 @@
+use std::collections::HashMap;
+
+fn main() {
+
+    let mut employees: HashMap<&str, &str> = HashMap::new();
+
+    let person1_id = "ab006587".to_string();
+
+    let person1_dep = "Finance".to_string();
+
+    employees.insert(&person1_id, &person1_dep);
+
+    employees.insert("ku009878", "HR");
+
+    println!("{employees:?}");
+
+    employees.insert("ku009878", "Sales Operations");
+
+    println!("{employees:?}");
+}
+
+// {"ab006587": "Finance", "ku009878": "HR"}
+// {"ab006587": "Finance", "ku009878": "Sales Operations"}
