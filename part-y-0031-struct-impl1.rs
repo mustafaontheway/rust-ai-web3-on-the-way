636d30eda3693a62f7a625a88fb0528816400b40
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-11-25 16:34:22 +0300

    Create part-y-0031-struct-impl1.rs

---
+++ part-y-0031-struct-impl1.rs
@@ -0,0 +1,30 @@
+fn main() {
+
+    let emp_bilge = Employee {
+        name: String::from("Bilge Kul"),
+        department_id: 99,
+        title: String::from("Expert"),
+        salary_tl: 62_300,
+        married: true   
+    };
+
+    emp_bilge.print_salary_info(); // Employee name: Bilge Kul - salary: 62300 ₺
+}
+
+
+#[derive(Debug)]
+struct Employee {
+    name: String,
+    department_id: u8,
+    title: String,
+    salary_tl: u32,
+    married: bool
+}
+
+impl Employee {
+
+    fn print_salary_info(&self) {
+        
+        println!("Employee name: {} - salary: {} ₺", self.name, self.salary_tl)
+    }
+}
