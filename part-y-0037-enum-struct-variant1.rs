42ddc0e19230952af1e2607dc406d452441f2632
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-12-02 19:41:14 +0300

    Create part-y-0037-enum-struct-variant1.rs

---
+++ part-y-0037-enum-struct-variant1.rs
@@ -0,0 +1,27 @@
+fn main() {
+
+    let mayor_ankara = Mayors {name: String::from("Mansur Yavaş"), vote_rate: 0.57}; // I'm not sure (0.57 ?)
+
+    let ankara = Cities::Ankara(mayor_ankara);
+
+    println!("{:?}", ankara); // Ankara(Mayors { name: "Mansur Yavaş", vote_rate: 0.57 })
+}
+
+#[derive(Debug)]
+enum Cities {
+
+    Ankara(Mayors),
+    Istanbul(Mayors),
+    Izmir(Mayors),
+}
+
+#[derive(Debug)]
+struct Mayors {
+
+    name: String,
+    vote_rate: f32
+}
+
+
+
+
