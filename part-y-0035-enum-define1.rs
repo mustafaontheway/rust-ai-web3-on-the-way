5c01f0002554b6108b15d620c5001ee24641fed9
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-12-02 18:09:08 +0300

    Create part-y-0035-enum-define1.rs

---
+++ part-y-0035-enum-define1.rs
@@ -0,0 +1,22 @@
+fn main() {
+
+    let capital = Cities::Ankara;
+
+    let mut her_city: Cities = Cities::Antalya;
+
+    her_city = Cities::Bursa;
+
+}
+
+enum Cities {
+
+    Ankara,
+    Istanbul,
+    Izmir,
+    Antalya,
+    Bursa,
+    Adana,
+    Kayseri
+}
+
+
