ad7005b4c9d25fdb95d96532dd67115a2bc3ef8f
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-12-10 20:01:59 +0300

    Create part-y-0061-ownership-function-parameters1.rs

---
+++ part-y-0061-ownership-function-parameters1.rs
@@ -0,0 +1,14 @@
+fn main() {
+
+    let her_name = String::from("Merve Günay");
+
+    print_name(her_name); // Your name is Merve Günay!
+
+    //println!("{her_name}") // Error! => borrow of moved value: `her_name`
+
+}
+
+fn print_name(name: String) {
+
+    println!("Your name is {name}!")
+}
