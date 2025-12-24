5049443c7d0081972cc977bfd7a7943f859a76da
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-12-24 09:27:33 +0300

    Create part-y-0069-break-continue2.rs

---
+++ part-y-0069-break-continue2.rs
@@ -0,0 +1,31 @@
+fn main() {
+
+    let mut year: u16 = 2025;
+
+    while year <= 2040 {
+
+        year += 1;
+
+        if year == 2030 {
+            continue;
+        }
+
+        if year == 2035 {
+            break;
+        }
+
+        println!("Year is {year}");
+    }
+
+    println!("Loop ended!");
+}
+
+// Year is 2026
+// Year is 2027
+// Year is 2028
+// Year is 2029
+// Year is 2031
+// Year is 2032
+// Year is 2033
+// Year is 2034
+// Loop ended! 
