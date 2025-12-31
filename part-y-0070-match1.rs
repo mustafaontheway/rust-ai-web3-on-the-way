1d840785c7e540264a9f13b3372dd4ca74c5d872
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-12-31 09:29:32 +0300

    Create part-y-0070-match1.rs

---
+++ part-y-0070-match1.rs
@@ -0,0 +1,13 @@
+fn main() {
+
+    let emp_id: u8 = 2;
+
+    match emp_id {
+        1 => println!("Ayhan Bilir"),
+        2 => println!("Bengü Burada"),
+        3 => println!("Kağan Büyükdere"),
+        _ => println!("Unknown employee & ID!")
+    }
+}
+
+// Bengü Burada
