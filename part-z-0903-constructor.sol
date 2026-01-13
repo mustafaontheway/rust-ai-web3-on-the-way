837a0410512db0d8279daf409edf9892b92cd25f
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-01-13 21:44:04 +0300

    Update part-z-0903-constructor.sol

--- part-z-0903-constructor.sol
+++ part-z-0903-constructor.sol
@@ -4,3 +4,16 @@ Create a constructor which will take a uint as an argument.
 Store this uint value inside a public storage variable called x.
 */
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Contract {
+
+    uint public x;
+
+    constructor(uint _x) {
+
+        x = _x;
+    }
+
+}
