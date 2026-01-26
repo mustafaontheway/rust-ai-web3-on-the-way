8578d5e874c5cb4d72ddd2852b339f3d535d88a3
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-01-26 22:40:01 +0300

    Create part-z-0501-week4-002-basic-types3.sol

---
+++ part-z-0501-week4-002-basic-types3.sol
@@ -0,0 +1,21 @@
+/*
+ Your Goal: Create Signed Integers!
+Create three public state integers a, b, and difference.
+
+Declare the variables a and b as int8. One of the values must be positive, the other must be negative.
+
+Declare the variable difference as a int16 which is the absolute difference between a and b.
+
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract Contract {
+
+    int8 public a = 126;
+
+    int8 public b = -100;
+
+    int16 public difference = a - b;   
+}
