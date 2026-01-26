92b75c549582c2e816d298ac151837d433e9009a
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-01-26 22:35:17 +0300

    Create part-z-0501-week4-002-basic-types1.sol

---
+++ part-z-0501-week4-002-basic-types1.sol
@@ -0,0 +1,16 @@
+/*
+Your Goal: Add two boolean variables
+Create two public boolean state variables on the contract: a and b.
+Store true in the variable a and false in the value b.
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract Contract {
+
+    bool public a = true;
+
+    bool public b = !a;
+	
+}
