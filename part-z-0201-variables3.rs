88d4471cf1e6e1321c3b23f09db81d15f22b6876
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-01-28 23:45:00 +0300

    Create part-z-0201-variables3.rs

---
+++ part-z-0201-variables3.rs
@@ -0,0 +1,13 @@
+// fn main() {
+//     // TODO: Change the line below to fix the compiler error.
+//     let x: i32;
+
+//     println!("Number {x}");
+// }
+
+fn main() {
+    // TODO: Change the line below to fix the compiler error.
+    let x: i32 = -50_000;
+
+    println!("Number {x}");
+}
