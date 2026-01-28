8f8b93c464b0c36e863071d1370c03acefa95bdc
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-01-28 23:43:54 +0300

    Create part-z-0201-variables2.rs

---
+++ part-z-0201-variables2.rs
@@ -0,0 +1,21 @@
+// fn main() {
+//     // TODO: Change the line below to fix the compiler error.
+//     let x;
+
+//     if x == 10 {
+//         println!("x is ten!");
+//     } else {
+//         println!("x is not ten!");
+//     }
+// }
+
+fn main() {
+    // TODO: Change the line below to fix the compiler error.
+    let x = 24;
+
+    if x == 10 {
+        println!("x is ten!");
+    } else {
+        println!("x is not ten!");
+    }
+}
