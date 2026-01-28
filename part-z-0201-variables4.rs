992735efccc9701b5e3eb6fce43c537a4ce1a350
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-01-28 23:45:56 +0300

    Create part-z-0201-variables4.rs

---
+++ part-z-0201-variables4.rs
@@ -0,0 +1,17 @@
+// // TODO: Fix the compiler error.
+// fn main() {
+//     let x = 3;
+//     println!("Number {x}");
+
+//     x = 5; // Don't change this line
+//     println!("Number {x}");
+// }
+
+// TODO: Fix the compiler error.
+fn main() {
+    let mut x = 3;
+    println!("Number {x}");
+
+    x = 5; // Don't change this line
+    println!("Number {x}");
+}
